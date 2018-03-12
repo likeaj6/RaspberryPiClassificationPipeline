015eaf31c63dfd579e1fa6e0cca440a3ff8461c4
Jason Jin <jasonjinhuan@gmail.com>
2018-03-12 03:08:08 -0500

    updating pipeline

--- pipeline.py
+++ pipeline.py
@@ -5,7 +5,7 @@ import numpy as np
 from datetime import datetime
 # from uploadFile import upload
 
-from label_image import infer_image, prep_numpy
+# from label_image import infer_image, prep_numpy
 
 import boto3
 
@@ -29,7 +29,7 @@ HEIGHT = 299
 IMAGE_DIRECTORY = './images/'
 
 def setUpCamera(camera):
-    camera.flash_mode = 'auto'
+    camera.flash_mode = 'on'
     camera.resolution = (WIDTH, HEIGHT)
 
 def getDateTime():
@@ -40,34 +40,35 @@ def convertStreamToNumpy(stream):
     stream.seek(0)
     # Calculate the actual image size in the stream (accounting for rounding
     # of the resolution)
-    fwidth = (width + 31) // 32 * 32
-    fheight = (height + 15) // 16 * 16
+    fwidth = (WIDTH + 31) // 32 * 32
+    fheight = (HEIGHT + 15) // 16 * 16
     # Load the data in a three-dimensional array and crop it to the requested
     # resolution
     image = np.fromfile(stream, dtype=np.uint8).\
-            reshape((fheight, fwidth, 3))[:height, :width, :]
+            reshape((fheight, fwidth, 3))[:HEIGHT, :WIDTH, :]
     # If you wish, the following code will convert the image's bytes into
     # floating point values in the range 0 to 1 (a typical format for some
     # sorts of analysis)
-    image = image.astype(np.float, copy=False)
+    image = image.astype(np.float32, copy=False)
     image = image / 255.0
+    print(type(image))
     return image
 
 def main():
     # Capture the image in RGB format
     filename = getDateTime() + '.jpg'
     stream = open(IMAGE_DIRECTORY + filename, 'w+b')
-    with tf.Session(graph=graph) as sess:
-        with picamera.PiCamera() as camera:
-            setUpCamera(camera)
-            camera.start_preview()
-            while True:
-                # time.sleep(5)
-                # livestream mode:
-                camera.capture(stream)
-                npImage = convertStreamToNumpy(stream)
-                preprocessed = prep_numpy(npImage)
-                infer_image(sess, input_operation, output_operation, preprocessed, WIDTH, HEIGHT)
-                upload(filename)
+    #with tf.Session(graph=graph) as sess:
+    with picamera.PiCamera() as camera:
+        setUpCamera(camera)
+        camera.start_preview()
+        #while True:
+        time.sleep(5)
+            # livestream mode:
+        camera.capture(stream)
+            # npImage = convertStreamToNumpy(stream)
+            # preprocessed = prep_numpy(npImage)
+            # infer_image(sess, input_operation, output_operation, preprocessed, WIDTH, HEIGHT)
+        upload(filename)
 
 main()
