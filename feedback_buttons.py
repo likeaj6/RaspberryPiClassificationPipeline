313f953dd796a294d2c36e79ba2ac2dde7e8aab2
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 08:45:14 -0400

    updating buttons

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -6,16 +6,20 @@ TRASH_PIN = 20
 RECYCLING_PIN = 16
 COMPOST_PIN = 21
 
+def button_callback(channel):
+    if channel == TRASH_PIN:
+        print('TRASH')
+    if channel == RECYCLING_PIN:
+        print('RECYCLING')
+    if channel == COMPOST_PIN:
+        print('COMPOST')
 
 GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    if not(GPIO.input(RECYCLING_PIN)):
-        print('RECYCLING')
-    if not(GPIO.input(TRASH_PIN)):
-        print("TRASH")
-    if not(GPIO.input(COMPOST_PIN)):
-        print("COMPOST")
+GPIO.add_event_detect(TRASH_PIN,GPIO.RISING,callback=button_callback)
+GPIO.add_event_detect(RECYCLING_PIN,GPIO.RISING,callback=button_callback)
+GPIO.add_event_detect(COMPOST_PIN,GPIO.RISING,callback=button_callback)
+
 GPIO.cleanup()
