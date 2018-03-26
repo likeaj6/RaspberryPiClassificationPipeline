993ce3db2fd438160fd3094ca34e49ebf0b0f93a
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 12:43:30 +0000

    working buttons

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -7,15 +7,15 @@ RECYCLING_PIN = 16
 COMPOST_PIN = 21
 
 
-GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
-    if not(GPIO.input(RECYCLING_PIN)):
+    if (GPIO.input(RECYCLING_PIN)):
         print('RECYCLING')
-    if not(GPIO.input(TRASH_PIN)):
+    if (GPIO.input(TRASH_PIN)):
         print("TRASH")
-    if not(GPIO.input(COMPOST_PIN)):
+    if (GPIO.input(COMPOST_PIN)):
         print("COMPOST")
 GPIO.cleanup()
