a5a523af7e82399b506bec6d8d888224ba6ad9ff
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 08:29:14 -0400

    updating buttons

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -2,20 +2,20 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+TRASH_PIN = 20
+RECYCLING_PIN = 16
+COMPOST_PIN = 21
 
 
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    button1 = GPIO.input(16)
-    if (not prev_input and button1):
-        print(button1)
-    if not(GPIO.input(20)):
-        print("20")
-    if not(GPIO.input(21)):
-        print("21")
-    prev_input = button1
-
+    if (not(GPIO.input(COMPOST_PIN)):
+        print('RECYCLING')
+    if not(GPIO.input(COMPOST_PIN)):
+        print("TRASH")
+    if not(GPIO.input(COMPOST_PIN)):
+        print("COMPOST")
 GPIO.cleanup()
