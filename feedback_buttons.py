19130d2fac34fbd92c027514ff3bc70cf1b73514
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 11:50:36 -0400

    removed event listener

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -6,24 +6,16 @@ TRASH_PIN = 20
 RECYCLING_PIN = 16
 COMPOST_PIN = 21
 
-def trash_callback(channel):
-    if channel == TRASH_PIN:
-        print('TRASH')
-def recycling_callback(channel):
-    if channel == RECYCLING_PIN:
+
+GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    if (not(GPIO.input(COMPOST_PIN)):
         print('RECYCLING')
-def compost_callback(channel):
-    if channel == COMPOST_PIN:
-        print('COMPOST')
-
-GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.add_event_detect(TRASH_PIN,GPIO.RISING,callback=trash_callback)
-GPIO.add_event_detect(RECYCLING_PIN,GPIO.RISING,callback=recycling_callback)
-GPIO.add_event_detect(COMPOST_PIN,GPIO.RISING,callback=compost_callback)
-
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-
+    if not(GPIO.input(COMPOST_PIN)):
+        print("TRASH")
+    if not(GPIO.input(COMPOST_PIN)):
+        print("COMPOST")
 GPIO.cleanup()
