7cec5a92e2bc50c17499d016b40ac22a7dfa3860
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 08:32:29 -0400

    adding erics ultrasonic sensor motion detection

--- motion_detection.py
+++ motion_detection.py
@@ -1,77 +1,102 @@
-# import numpy as np
-# import picamera
-import RPi.GPIO as GPIO
+#!/usr/bin/env python
+
+# Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
+# This code is released into the public domain
 
 import time
-
-def measure():
-    # This function measures a distance
-    GPIO.output(TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIGGER, False)
-    start = time.time()
-
-    while GPIO.input(ECHO) == 0:
-        start = time.time()
-
-    while GPIO.input(ECHO)==1:
-        stop = time.time()
-
-    elapsed = stop-start
-    distance = (elapsed * 34300)/2
-
-    return distance
-
-def measure_average():
-    # This function takes 3 measurements and
-    # returns the average.
-    distance1=measure()
-    time.sleep(0.1)
-    distance2=measure()
-    time.sleep(0.1)
-    distance3=measure()
-    distance = distance1 + distance2 + distance3
-    distance = distance / 3
-    return distance
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
+DEBUG = 0
 
-TRIGGER = 21
-ECHO = 20
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
 
-GPIO.setup(TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(ECHO,GPIO.IN)      # Echo
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
 
-# Set trigger to False (Low)
-# GPIO.output(TRIGGER, False)
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
 
-# Wrap main content in a try block so we can
-# catch the user pressing CTRL-C and run the
-# GPIO cleanup function. This will also prevent
-# the user seeing lots of unnecessary error
-# messages.
-try:
-    while True:
-        distance = measure_average()
-        print("Distance: ", distance)
-        time.sleep(1)
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
 
-except KeyboardInterrupt:
-    # User pressed CTRL-C
-    # Reset GPIO settings
-    GPIO.cleanup()
+        GPIO.output(cspin, True)
 
-# In case getting Ultrasonic Sensors to work is too much of a hassle, we'll use built in crude analysis based on frames
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
 
-# class FrameMotionDetector(picamera.array.PiMotionAnalysis):
-#     def analyse(self, a):
-#         a = np.sqrt(
-#             np.square(a['x'].astype(np.float)) +
-#             np.square(a['y'].astype(np.float))
-#             ).clip(0, 255).astype(np.uint8)
-#         # If there're more than 10 vectors with a magnitude greater
-#         # than 60, then say we've detected motion
-#         if (a > 60).sum() > 10:
-#             print('Motion detected!')
-#             stream = open('image' + '.data', 'w+b')
-#             camera.capture(stream, 'rgb')
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# 10k trim pot connected to adc #0
+ultra_adc = 0;
+
+last_read = 0       # this keeps track of the last potentiometer value
+tolerance = 20       # to keep from being jittery we'll only change
+                    # volume when the pot has moved more than 5 'counts'
+
+def read_ultra_inches():
+    return readadc(ultra_adc, SPICLK, SPIMOSI, SPIMISO, SPICS) / 9.8
+
+if __name__ == "__main__":
+        while True:
+                # assume no change
+                changed = False
+
+                # read the analog pin
+                read = readadc(ultra_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+                # how much has it changed since the last read?
+                pot_adjust = abs(read - last_read)
+
+                if DEBUG:
+                        print "read:", read
+                        print "pot_adjust:", pot_adjust
+                        print "last_read", last_read
+
+                if ( pot_adjust > tolerance ):
+                       changed = True
+
+                if DEBUG:
+                        print "changed", changed
+
+                if ( changed or True):
+                        adjusted = read / 10.24             # convert 10bit adc0 (0-1024) read into 0-100
+                        adjusted = round(adjusted)          # round out decimal value
+                        adjusted = int(adjusted)            # cast as integer
+
+                        print read
+
+                        # save the potentiometer reading for the next loop
+                        last_read = read
+
+                # hang out and do nothing for a half second
+                time.sleep(0.5)
