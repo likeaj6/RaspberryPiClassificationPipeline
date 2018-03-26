7d9b799f9cc4fec29321b64ab39ac614829323eb
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 08:48:26 -0400

    added button event detection

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -6,11 +6,13 @@ TRASH_PIN = 20
 RECYCLING_PIN = 16
 COMPOST_PIN = 21
 
-def button_callback(channel):
+def trash_callback(channel):
     if channel == TRASH_PIN:
         print('TRASH')
+def recycling_callback(channel):
     if channel == RECYCLING_PIN:
         print('RECYCLING')
+def compost_callback(channel):
     if channel == COMPOST_PIN:
         print('COMPOST')
 
@@ -18,8 +20,10 @@ GPIO.setup(TRASH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(RECYCLING_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(COMPOST_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(TRASH_PIN,GPIO.RISING,callback=button_callback)
-GPIO.add_event_detect(RECYCLING_PIN,GPIO.RISING,callback=button_callback)
-GPIO.add_event_detect(COMPOST_PIN,GPIO.RISING,callback=button_callback)
+GPIO.add_event_detect(TRASH_PIN,GPIO.RISING,callback=trash_callback)
+GPIO.add_event_detect(RECYCLING_PIN,GPIO.RISING,callback=recycling_callback)
+GPIO.add_event_detect(COMPOST_PIN,GPIO.RISING,callback=compost_callback)
+
+message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 GPIO.cleanup()
