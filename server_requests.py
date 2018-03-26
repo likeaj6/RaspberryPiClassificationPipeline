c38796c1be9f8eb997f9a66575b638e6ab6814af
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 13:05:38 -0400

    added server requests

---
+++ server_requests.py
@@ -0,0 +1,16 @@
+import requests
+import os
+
+URL = os.environ['SERVER_URL']
+PI_ID = os.environ['PI_ID']
+
+def motionDetectedRequest():
+    res = requests.get(URL + '/detected/' + PI_ID)
+    return res
+
+def buttonFeedbackRequest(classification):
+    if classification in ['Trash', 'Recycle', 'Compost']:
+        res = requests.get(URL + '/feedback/' + PI_ID + '?classification=' + classification)
+        return res
+
+motionDetectedRequest()
