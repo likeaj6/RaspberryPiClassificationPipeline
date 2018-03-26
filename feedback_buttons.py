d0053bf8a8bddafa5f50c4c91530f03b1c429988
Jason Jin <jasonjinhuan@gmail.com>
2018-03-26 12:26:49 +0000

    testing buttons

--- feedback_buttons.py
+++ feedback_buttons.py
@@ -1,14 +1,21 @@
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+
+
+
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
-    if not(GPIO.input(16)):
-        print("16")
+    button1 = GPIO.input(16)
+    if (not prev_input and button1):
+        print(button1)
     if not(GPIO.input(20)):
         print("20")
     if not(GPIO.input(21)):
         print("21")
+    prev_input = button1
 
 GPIO.cleanup()
